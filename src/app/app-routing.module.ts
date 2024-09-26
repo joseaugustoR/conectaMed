09ae401a4e938e33bef5b0a6fb88ea5954f74e9f
Brendaunisuam22 <brendaroliveira@souunisuam.com.br>
2024-09-26 19:00:55 -0300

    resolução

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,29 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },  {
     path: 'criadores',
-    loadChildren: () => import('./criadores/criadores.module').then( m => m.CriadoresPageModule)
+    loadChildren: () => import('./criadores/criadores.module').then(m => m.CriadoresPageModule)
   },
-<<<<<<< HEAD
-=======
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-<<<<<<< HEAD
-=======
-
->>>>>>> 75fca55a888c2f8fb8cedcb8409d6bf44b3817c5
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'cadastro',
-    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+    loadChildren: () => import('./cadastro/cadastro.module').then(m => m.CadastroPageModule)
   },
-
-];
->>>>>>> 75fca55a888c2f8fb8cedcb8409d6bf44b3817c5
-
-<<<<<<< HEAD
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
@@ -53,13 +33,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-=======
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> 52b86c93f99c50d8eb847ec9e74dc27f241b6967
