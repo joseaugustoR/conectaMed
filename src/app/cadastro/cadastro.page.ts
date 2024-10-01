6074ac432958d095367cf37966fffa21e4bc2b80
José Augusto <joseaugustorodrigues440@gmail.com>
2024-10-01 09:34:20 -0300

    testes

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { UsuarioModel } from 'src/models/usuario.models';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
 })
+
 export class CadastroPage implements OnInit {
 
   constructor() { }
@@ -12,4 +16,16 @@ export class CadastroPage implements OnInit {
   ngOnInit() {
   }
 
+}
+
+export class HomePage {
+  userForm: FormGroup;
+
+  constructor(public form: FormBuilder, public navCtrl: NavController) {
+    this.userForm = this.form.group ({
+        nome: ['', Validators.required],
+        email: ['', Validators.required]
+    })
+
+  }
 }
