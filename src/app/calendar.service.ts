c832d458571db8ee3b585dfd8fcfe7335ac1a747
Brendaunisuam22 <167194612+Brendaunisuam22@users.noreply.github.com>
2024-11-02 14:40:43 -0300

    atts

--- src/app/calendar.service.ts
+++ src/app/calendar.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
 import { gapi } from 'gapi-script';
-import { CalendarEvent } from './calendar-event.model'; // Ajuste o caminho
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
-  async getEvents(): Promise<CalendarEvent[]> { // Tipando o retorno
+  async getEvents(): Promise<any[]> {
     return new Promise((resolve, reject) => {
       gapi.client.load('calendar', 'v3', async () => {
         try {
           const response = await gapi.client.calendar.events.list({
             calendarId: 'primary',
-            timeMin: (new Date()).toISOString(),
+            timeMin: new Date().toISOString(),
             maxResults: 10,
             singleEvents: true,
             orderBy: 'startTime'
           });
-          resolve(response.result.items || []); // Garante que seja um array
+          resolve(response.result.items || []);
         } catch (error) {
           console.error('Erro ao buscar eventos:', error);
           reject(error);
