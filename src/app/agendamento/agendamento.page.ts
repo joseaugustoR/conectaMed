3becc8f638b0aaa7a229228e65d3f5c0870500fa
Brendaunisuam22 <brendaroliveira@souunisuam.com.br>
2024-11-14 16:02:41 -0300

    agendamento

--- src/app/agendamento/agendamento.page.ts
+++ src/app/agendamento/agendamento.page.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendamentoPage implements OnInit {
 
+  consulta: string = '';
+  local: string = '';
+  data: string = '';
+  hora: string = '';
+
+  informacoes: { consulta: string; local: string; data: string; hora: string } | null = null;
+  mensagemErro: string = ''; // Variável para armazenar a mensagem de erro
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  // Função chamada ao clicar no botão de confirmar agendamento
+  exibirInformacoes() {
+    // Verifica se todos os campos foram preenchidos
+    if (!this.consulta || !this.local || !this.data || !this.hora) {
+      this.mensagemErro = 'Por favor, preencha todos os campos.'; // Exibe mensagem de erro
+      this.informacoes = null; // Limpa as informações anteriores, se houver
+    } else {
+      this.mensagemErro = ''; // Limpa a mensagem de erro, se todos os campos estiverem preenchidos
+      this.informacoes = {
+        consulta: this.consulta,
+        local: this.local,
+        data: this.data,
+        hora: this.hora
+      };
+    }
+  }
 }
